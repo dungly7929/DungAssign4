457810709888c028c54767103f3d10fba3445793
dungly7929 <danks76429@gmail.com>
2021-04-11 21:36:04 -0400

    Fragment Settings with Setting change for Home Fragment (Background and Clock) commit !

--- DungAssign44/src/main/java/dung/ly/n01327929/SettingsFrag.java
+++ DungAssign44/src/main/java/dung/ly/n01327929/SettingsFrag.java
@@ -5,30 +5,39 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SettingsFrag extends Fragment
 {
     SharedPreferences sharedPreferences;
     View view;
     Button btnapply;
-    RadioGroup btngroup;
-    RadioButton format12, format24;
+    RadioGroup btngroup, btngroup1;
+    RadioButton format12, format24, pink, white, purple;
+    Switch swtori;
+    boolean orisw = false;
 
     private void setupid()
     {
         format12 = (RadioButton) view.findViewById(R.id.radiobtn12h);
         format24 = (RadioButton) view.findViewById(R.id.radiobtn24h);
         btnapply = (Button) view.findViewById(R.id.btnsettingapply);
+        pink = (RadioButton) view.findViewById(R.id.radiobtnpink);
+        white = (RadioButton) view.findViewById(R.id.radiobtnwhite);
+        purple = (RadioButton) view.findViewById(R.id.radiobtnpurple);
         btngroup = (RadioGroup) view.findViewById(R.id.timebtngroup);
+        btngroup1 = (RadioGroup) view.findViewById(R.id.bgbtngroup);
+        swtori = (Switch) view.findViewById(R.id.orientationsw);
     }
 
     @Override
@@ -39,30 +48,57 @@ public class SettingsFrag extends Fragment
         setupid();
         sharedPreferences = this.getActivity().getSharedPreferences("Data", Context.MODE_PRIVATE);
         String timeformat = sharedPreferences.getString("time", "24h");
-        if(btngroup.getCheckedRadioButtonId() == -1)
+        int bgcolor = sharedPreferences.getInt("bgcolor", 0);
+        boolean orisw1 = sharedPreferences.getBoolean("orisw", false);
+        if (!orisw1)
         {
-            if(timeformat.equals("12h"))
+            swtori.setChecked(false);
+        } else
+        {
+            swtori.setChecked(true);
+        }
+        if (btngroup.getCheckedRadioButtonId() == -1)
+        {
+            if (timeformat.equals("12h"))
             {
                 format12.setChecked(true);
-            }
-            else if(timeformat.equals("24h"))
+            } else if (timeformat.equals("24h"))
             {
                 format24.setChecked(true);
             }
         }
 
-        btnapply.setOnClickListener(v ->
+        if (btngroup1.getCheckedRadioButtonId() == -1)
         {
-            sendData();
+            if (bgcolor == 1)
+            {
+                purple.setChecked(true);
+            } else if (bgcolor == 2)
+            {
+                white.setChecked(true);
+            } else if (bgcolor == 3)
+            {
+                pink.setChecked(true);
+            }
+        }
 
+        swtori.setOnCheckedChangeListener((buttonView, isChecked) ->
+        {
+            orisw = isChecked;
         });
+
+            btnapply.setOnClickListener(v ->
+            {
+                sendData();
+
+            });
         return view;
     }
 
     private void sendData()
     {
+        //Change time format
         String timeformat = "";
-        SharedPreferences.Editor editor = sharedPreferences.edit();
         if (format12.isChecked())
         {
             timeformat = format12.getText().toString();
@@ -70,7 +106,23 @@ public class SettingsFrag extends Fragment
         {
             timeformat = format24.getText().toString();
         }
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("time", timeformat);
+
+        //Change background color
+        int bgcolor = 0;
+        if (pink.isChecked())
+        {
+            bgcolor = 3;
+        } else if (white.isChecked())
+        {
+            bgcolor = 2;
+        } else
+        {
+            bgcolor = 1;
+        }
+        editor.putInt("bgcolor", bgcolor);
+        editor.putBoolean("orisw",orisw);
         editor.apply();
     }
 }
